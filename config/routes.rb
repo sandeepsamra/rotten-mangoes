0c2febbaee7bc3880104945a3648d6ec0919338c
Sandeep Samra <ssamra67@gmail.com>
2016-02-01 17:41:13 -0500

    reviews controller & reviews/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RottenMangoes::Application.routes.draw do
+  get "reviews/new"
+  get "reviews/create"
   get "sessions/new"
   get "sessions/create"
   get "users/new"
@@ -8,8 +10,10 @@ RottenMangoes::Application.routes.draw do
   get "movies/new"
   get "movies/edit"
 
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
