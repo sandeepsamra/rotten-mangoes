6c992bba5b336dbb583fa6d1b1fd7be16581840b
Sandeep Samra <ssamra67@gmail.com>
2016-02-03 16:49:05 -0500

    email user when account deleted

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,18 @@
 class Admin::UsersController < ApplicationController
 
   def index
-    @users = User.all.page(params[:page]).per(1)
+    @users = User.all.page(params[:page]).per(2)
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    UserMailer.delete_email(@user).deliver
+    @user.destroy
+    redirect_to admin_users_path
   end
 
 end
