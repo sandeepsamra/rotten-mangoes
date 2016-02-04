bc5cc1d4aec385d27492e0c34fb2e9816aff4124
Sandeep Samra <ssamra67@gmail.com>
2016-02-04 18:21:06 -0500

    fixed better search form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,13 @@ class MoviesController < ApplicationController
   #   @movies = Movie.duration(params[:runtime_in_minutes]).search(params[:title], params[:director])
   # end
 
+  # def index
+  #   if params[:runtime_in_minutes] || params[:title] || params[:director]
+  #     @movies = Movie.duration(params[:runtime_in_minutes]).search(params[:title], params[:director])
+  #   end
+  #   @movies = Movie.all
+  # end
+
   def index
     unless params[:search]
       @movies = Movie.all
